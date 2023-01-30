5f4fb469ebe2eda2dd9cde1ec1bbfe5d49ade215
junruizhang <junrui@junruizhangdeMacBook-Pro.local>
2023-01-30 21:58:54 +0800

    feat: 增加commit配置文件

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 function App() {
   const [count, setCount] = useState(0);
-  console.log('count', count);
+  console.log('count =>', count);
 
   return (
     <div>
