71f253a99fe1a2e64752d6ae1e27d79357fb28a8
junruizhang <junrui@junruizhangdeMacBook-Pro.local>
2023-01-30 21:09:18 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 function App() {
   const [count, setCount] = useState(0);
+  console.log('count', count);
 
   return (
     <div>
